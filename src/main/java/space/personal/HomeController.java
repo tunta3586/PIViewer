a9df80a161c46aebce84bdbef501cbc8eed17ca4
a024148 <tunta3586@gmail.com>
2023-10-18 20:11:52 +0900

    init

---
+++ src/main/java/space/personal/HomeController.java
@@ -0,0 +1,14 @@
+package space.personal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/index")
+    public String hello(Model model) {
+        return "index";
+    }
+}
